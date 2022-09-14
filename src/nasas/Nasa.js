20152f4d2e6c08fcafea76059813fbca6bbb143b
LOL 228 <nursultanr585@gmail.com>
2022-09-14 15:55:41 +0600

    nasa api

--- src/nasas/Nasa.js
+++ src/nasas/Nasa.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const Nasa = () => {
     let rovers = [
         {
@@ -5,33 +7,68 @@ const Nasa = () => {
             cameras: ['fhaz', 'rhaz', 'mast', 'chemcam', 'mahli', 'mardi', 'navcam']
         },
         {
-            name: 'oppotrunity',
-            cameras: ['fhaz', 'rhaz','navcam', 'pancam', 'minites']
+            name: 'opportunity',
+            cameras: ['fhaz', 'rhaz', 'navcam', 'pancam', 'minites']
         },
         {
             name: 'spirit',
-            cameras: ['fhaz', 'rhaz','navcam', 'pancam', 'minites']
+            cameras: ['fhaz', 'rhaz', 'navcam', 'pancam', 'minites']
         },
     ]
-    let firstSelect = document.querySelector('.firstSelect')
-    const selects = () => {
-        // for(let i = 0)
-        // for(let item of rovers){
-        //     firstSelect.append(`<option value='${item.name}'>${item.name.toUpperCase()}</option>`)
-        // }
-    }
-    selects()
-    const onShow = () => {
+    const [src, setSrc] = useState('')
+    const [cameras, setCameras] = useState(rovers[0].cameras)
 
+    const onShow = () => {
+        const firstSelect = document.querySelector('.firstSelect')
+
+        const secSelect = document.querySelector('.secSelect')
+        const input_val = document.querySelector('input').value
+
+        const key = 'wEI4nOiVivQHOjbmhcCypzM90CSG8S3HpHIb0wGK'
+        console.log(secSelect.value)
+        console.log(firstSelect.value)
+
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${firstSelect.value}/photos?sol=${input_val}&camera=${secSelect.value}&api_key=${key}`)
+            .then((response) => response.json())
+            .then((data) => setSrc(data.photos[0].img_src))
+            // .then((data) => console.log(data))
+            .catch(error => {
+                throw (error)
+            })
     }
-    return(
+    const onEdit = () => {
+        const firstSelect = document.querySelector('.firstSelect')
+
+        if (firstSelect.value == 'curiosity') {
+            setCameras(rovers[0].cameras)
+        } else if (firstSelect.value = 'opportunity') {
+            setCameras(rovers[1].cameras)
+
+        } else if(firstSelect.value = 'spirit'){
+            setCameras(rovers[2].cameras)
+        }else{
+            console.log('s')
+        }
+    }
+    return (
         <div>
-            <select className='firstSelect'>
-                <option value={rovers[0]}></option>
+            <select className='firstSelect' onChange={onEdit}>
+                {/* <option value={rovers[0].name}></option> */}
+                {rovers.map((item) => <option>{item.name}</option>)}
             </select>
-            <select></select>
-            <input type='number'/>
+            <select className='secSelect' >
+                {/* {rovers.map((item) => <div>{rovers.map((item) => <div>{rovers.map}</div>)}</div>)} */}
+                {/* {rovers.map((item) => <option>{item.cameras}</option>)} */}
+                {cameras.map(camera => (
+                    <option>{camera}</option>
+                ))}
+            </select>
+            <input type='number' />
             <button onClick={onShow}>show</button>
+            <div>
+
+                <img src={src} style={{ width: 200 }} />
+            </div>
         </div>
     )
 }
